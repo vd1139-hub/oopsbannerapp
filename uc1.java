f2517f3190f642d0cbe4f33a4421a763815ae9e5
mahesh kumar <vd1139@srmist.edu.in>
2026-03-11 10:40:47 +0530

    feature/uc1

---
+++ uc1.java
@@ -0,0 +1,10 @@
+public class OOPSBannerApp {
+
+    // Main method - program execution starts here
+    public static void main(String[] args) {
+
+        // Printing text to console
+        System.out.println("OOPS");
+
+    }
+}
